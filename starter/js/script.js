f2e0730f1049a8a89cc3fbf6b9a5e7dc9c370be1
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-13 12:08:38 +0000

    changed 2nd fetch into async function, working on day extraction

--- starter/js/script.js
+++ starter/js/script.js
@@ -2,9 +2,8 @@ $(function(){
     const apiKey = 'cd37f59da5ea1678108b4a3eacf1b443';
     const todayDt = dayjs().format('DD/M/YYYY');
     const todayNewFormat = dayjs().format('YYYY-MM-DD');
-    const tomorrow = todayNewFormat + 1;
+    // !print out today's date in same format as used by API
     console.log(todayNewFormat);
-    console.log(tomorrow);
     // add click listener on button
     $("#search-button").click(function(event) {
         event.preventDefault();
@@ -23,44 +22,88 @@ $(function(){
         })
         .then(function (data) {
             // fetch weather data
-            const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${apiKey}`;
-            fetch(weatherQueryUrl)
-            .then(function (response) {
-            return response.json()
-            })
-            .then(function (data) {
-            console.log(data)
-            // extract today's weather
-            let cityHeader = $('<h4>').text(`${properCaseCityName} (${todayDt})`);
-            let todayTemp = $('<p>').text(`${Math.round(data.list[0].main.temp)}°C`);
-            let todayWind = $('<p>').text(`${Math.round(data.list[0].wind.speed)} m/s`);
-            let todayHumidity = $('<p>').text(`${data.list[0].main.humidity}%`);
-            // clear today's display and add today weather
-            $('#today').empty();
-            $('#today').append(cityHeader, todayTemp, todayWind, todayHumidity);
-            // add search city to history
-            const lastCity = $('<button>').text(properCaseCityName);
-            $('#history').append(lastCity);
-            // TODO: add delegated click listener on #history to fetch, add today's weather and forecast
-            console.log(data.list[0].dt_txt);
-            // const 
-            try {
-                const dateArr = data.list;
-                $.each(dateArr, (i) => {
-                    let date = dateArr[i].dt_txt;
-                    console.log(date);
-                })
-            } catch(error) {
-                console.log("Error:", error);
-            }
-            // const forecastDate = data.list[0].dt_txt;
-            // console.log(forecastDate);
-            // todo: extract 5 day forecast
+            const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}&units=metric`;
+            // !print out the query url
+            console.log(weatherQueryUrl);
+            getWeatherData();
 
+            async function getWeatherData() {
+                try {
+                    const response = await fetch(weatherQueryUrl);
+                    const data = await response.json();
+                    console.log(data);
+                    let cityHeader = $('<h4>').text(`${properCaseCityName} (${todayDt})`);
+                    let todayTemp = $('<p>').text(`${Math.round(data.list[0].main.temp)}°C`);
+                    let todayWind = $('<p>').text(`${Math.round(data.list[0].wind.speed)} m/s`);
+                    let todayHumidity = $('<p>').text(`${data.list[0].main.humidity}%`);
+                    // clear today's display and add today weather
+                    $('#today').empty();
+                    $('#today').append(cityHeader, todayTemp, todayWind, todayHumidity);
+                    // add search city to history
+                    const lastCity = $('<button>').text(properCaseCityName);
+                    $('#history').append(lastCity);
+                    // TODO: add delegated click listener on #history to fetch, add today's weather and forecast
+                    console.log(data.list[0].dt_txt);
+                    // print out 
+                    try {
+                        const dateArr = data.list;
+                        $.each(dateArr, (i) => {
+                            let fullArrDate = dateArr[i].dt_txt;
+                            // !print out the full date and time as used in API
+                            console.log(fullArrDate);
+
+                            let dayObj = new Date(fullArrDate);
+                            const dayOnly = dayObj.getDate();
+                            // !print out just the day from the time stamp in API
+                            console.log(dayOnly);
+                        })
+                        } catch(error) {
+                            console.log("Error:", error);
+                        }
+                    } catch(err) {
+                        console.log("ERROR!", err);
+                    }
+                }
+
+            // fetch(weatherQueryUrl)
+            // .then(function (response) {
+            // return response.json()
+            // })
+            // .then(function (data) {
+            // console.log(data)
+            // extract today's weather
+            // let cityHeader = $('<h4>').text(`${properCaseCityName} (${todayDt})`);
+            // let todayTemp = $('<p>').text(`${Math.round(data.list[0].main.temp)}°C`);
+            // let todayWind = $('<p>').text(`${Math.round(data.list[0].wind.speed)} m/s`);
+            // let todayHumidity = $('<p>').text(`${data.list[0].main.humidity}%`);
+            // // clear today's display and add today weather
+            // $('#today').empty();
+            // $('#today').append(cityHeader, todayTemp, todayWind, todayHumidity);
+            // // add search city to history
+            // const lastCity = $('<button>').text(properCaseCityName);
+            // $('#history').append(lastCity);
+            // // TODO: add delegated click listener on #history to fetch, add today's weather and forecast
+            // console.log(data.list[0].dt_txt);
+            // // const 
+            // try {
+            //     const dateArr = data.list;
+            //     $.each(dateArr, (i) => {
+            //         let fullArrDate = dateArr[i].dt_txt;
+            //         console.log(fullArrDate);
+            //         let dayObj = new Date(fullArrDate);
+            //         const dayOnly = dayObj.getDate();
+            //         console.log(dayOnly);
+            //     })
+            // } catch(error) {
+            //     console.log("Error:", error);
+            // }
+            
+            // todo: extract 5 day forecast
+            // day 1: 
             // todo: clear previous and display forecast
             
             })
 
         })
     })
-})
+// })
