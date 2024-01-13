08dbe7ceb42a51201f2975a91348066101cc0f6b
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-13 09:39:16 +0000

    added Proper Case for city name; array for dates

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,12 +1,21 @@
 $(function(){
     const apiKey = 'cd37f59da5ea1678108b4a3eacf1b443';
     const todayDt = dayjs().format('DD/M/YYYY');
+    const todayNewFormat = dayjs().format('YYYY-MM-DD');
+    const tomorrow = todayNewFormat + 1;
+    console.log(todayNewFormat);
+    console.log(tomorrow);
     // add click listener on button
     $("#search-button").click(function(event) {
         event.preventDefault();
-        // 1 extract city from the input field
+        // extract city from the input field
         const cityName = $('#search-input').val().trim();
+        // make sure city name is in Proper Case
+        const properCaseCityName = cityName.toLowerCase().replace(/\b[a-z]/g, function(txtVal) {
+            return txtVal.toUpperCase();
+        });
         // get city's latitude and longitude
+        // code from option 4 in https://www.smartherd.com/convert-text-cases-using-jquery-without-css/
         const geoQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
         fetch(geoQueryUrl)
         .then(function (response) {
@@ -22,7 +31,7 @@ $(function(){
             .then(function (data) {
             console.log(data)
             // extract today's weather
-            let cityHeader = $('<h4>').text(`${cityName} (${todayDt})`);
+            let cityHeader = $('<h4>').text(`${properCaseCityName} (${todayDt})`);
             let todayTemp = $('<p>').text(`${Math.round(data.list[0].main.temp)}°C`);
             let todayWind = $('<p>').text(`${Math.round(data.list[0].wind.speed)} m/s`);
             let todayHumidity = $('<p>').text(`${data.list[0].main.humidity}%`);
@@ -30,45 +39,28 @@ $(function(){
             $('#today').empty();
             $('#today').append(cityHeader, todayTemp, todayWind, todayHumidity);
             // add search city to history
-            const lastCity = $('<button>').text(cityName);
+            const lastCity = $('<button>').text(properCaseCityName);
             $('#history').append(lastCity);
+            // TODO: add delegated click listener on #history to fetch, add today's weather and forecast
+            console.log(data.list[0].dt_txt);
+            // const 
+            try {
+                const dateArr = data.list;
+                $.each(dateArr, (i) => {
+                    let date = dateArr[i].dt_txt;
+                    console.log(date);
+                })
+            } catch(error) {
+                console.log("Error:", error);
+            }
+            // const forecastDate = data.list[0].dt_txt;
+            // console.log(forecastDate);
+            // todo: extract 5 day forecast
 
+            // todo: clear previous and display forecast
+            
             })
 
         })
     })
-
-
 })
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    // fetch request from OpenWeather API
-    // fetch(apiUrl)
-    // .then(response => response.json())
-    // .then(data => {
-    //     // Handle the data from the API
-    //     console.log(data);
-    // })
-    // .catch(error => {
-    //     // Handle errors
-    //     console.error('Error fetching data:', error);
-    // });
-
-    // 4 extract data, create a button with city name, add a listener to the button
-
-    // 5 display current weather in the box
-
-    // 6 add 5 cards with 5 day forecast
-
-    // 7 add a listener to the buttons to run fetch request and update 
-    // })
