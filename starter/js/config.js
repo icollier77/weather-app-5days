9a103a9fee3ec0ac8534e837894cef578209378f
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-15 17:19:54 +0000

    removed obsolete reference to country

--- starter/js/config.js
+++
@@ -1,3 +0,0 @@
-var config = {
-    apiKey: cd37f59da5ea1678108b4a3eacf1b443
-};
--- starter/js/script.js
+++ starter/js/script.js
@@ -67,12 +67,13 @@ async function getWeather(location, key) {
             addToLocalStorage(location);
             // Fetch weather data
             const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${key}&units=metric`;
-            getWeatherData(weatherQueryUrl, location, countryCode);
+            getWeatherData(weatherQueryUrl, location);
         } else if (data.length == 0) {
             // TODO: change to a modal
             alert("Please enter a valid location!");
         }
     } catch (err) {
+        // ! STILL ADDS INVALID CITY
         // TODO: change to a modal
         alert("Issues with API request, or invalid location!");
         console.log("ERROR with GEO data:", err);
